411e68a651131e8e891060f1ff51a4566138bff5
Vikesh667 <114086050+Vikesh667@users.noreply.github.com>
2023-05-18 09:36:18 +0530

    Clouser

---
+++ clouser.js
@@ -0,0 +1,47 @@
+// Closures
+function counter() {
+    let count = 0;
+  
+    return function() {
+      count++; 
+      return count;
+    };
+  }
+  
+  
+  const counter1 = counter();
+  const counter2 = counter();
+  
+  
+  console.log(counter1()); 
+  console.log(counter1()); 
+  console.log(counter1()); 
+  
+  console.log(counter2()); 
+  console.log(counter2()); 
+  console.log(counter2()); 
+  console.log(counter2()); 
+  console.log(counter2()); 
+
+
+// Higher-Order Functions
+  function calculate(num1, num2, callback) {
+    return callback(num1, num2);
+  }
+  
+  function add(num1, num2) {
+    return num1 + num2;
+  }
+  
+  function subtract(num1, num2) {
+    return num1 - num2;
+  }
+  
+  function multiply(num1, num2) {
+    return num1 * num2;
+  }
+  
+  
+  console.log(calculate(5, 3, add)); 
+  console.log(calculate(10, 4, subtract)); 
+  console.log(calculate(7, 2, multiply)); 
